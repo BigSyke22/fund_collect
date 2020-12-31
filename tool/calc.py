0d8ee2629902ec0bac4bf2566b33f5adbbff51a5
Zhang <syke.zhang@sap.com>
2020-12-31 15:24:52 +0800

    modify readme

---
+++ tool/calc.py
@@ -0,0 +1,27 @@
+import math
+from sys import argv
+def calc_value(years, investment, salary, salary_growth, inflation):
+    duration_month = years * 12
+
+    value = 0
+
+    grow_monthly = pow(investment + 1, 1/12)
+    #print(grow_monthly)
+    salary_monthly = salary
+    for time in range(0, duration_month):
+        if 0 == time % 12 and 0 != time:
+            salary_monthly = salary_monthly * (1 + salary_growth)
+
+        value_this_priod = pow(grow_monthly, duration_month - time) * salary_monthly
+        value += value_this_priod
+        #print(value)
+
+    print("current value considered inflation", int(value/ pow((1 + inflation), years)))
+
+if __name__ == '__main__':
+    years = int(argv[1])
+    investment = float(argv[2])
+    salary = int(argv[3])
+    salary_growth = float(argv[4])
+    inflation = float(argv[5])
+    calc_value(years, investment, salary, salary_growth, inflation)
