9c6d337fa62ab7da7ccd9f6472fd0865c18b03ed
Zhang <syke.zhang@sap.com>
2021-02-18 16:30:53 +0800

    add bonus

--- tool/calc.py
+++ tool/calc.py
@@ -1,6 +1,6 @@
 import math
 from sys import argv
-def calc_value(years, investment, salary, salary_growth, inflation):
+def calc_value(years, investment, salary, salary_growth, inflation, bonus):
     duration_month = years * 12
 
     value = 0
@@ -8,18 +8,24 @@ def calc_value(years, investment, salary, salary_growth, inflation):
     grow_monthly = pow(investment + 1, 1/12)
     #print(grow_monthly)
     salary_monthly = salary
+    bonus_current = bonus
     for time in range(0, duration_month):
         if 0 == time % 12 and 0 != time:
             salary_monthly = salary_monthly * (1 + salary_growth)
+            bonus_current = bonus_current * (1 + salary_growth)
 
-        value_this_priod = pow(grow_monthly, duration_month - time) * salary_monthly
+            value_this_priod = pow(grow_monthly, duration_month - time) * (salary_monthly + bonus_current)
+        
+        else:
+            value_this_priod = pow(grow_monthly, duration_month - time) * salary_monthly
+
         value += value_this_priod
         #print(value)
 
     print("current value considered inflation", int(value/ pow((1 + inflation), years)))
 
 if __name__ == '__main__':
-    if len(argv) < 6:
+    if len(argv) != 6:
         print("Example Insert: 10 0.2 15000 0.12 0.08")
         quit()
         
@@ -28,4 +34,5 @@ if __name__ == '__main__':
     salary = int(argv[3])
     salary_growth = float(argv[4])
     inflation = float(argv[5])
-    calc_value(years, investment, salary, salary_growth, inflation)
+    bonus = salary * 8
+    calc_value(years, investment, salary, salary_growth, inflation, bonus)
