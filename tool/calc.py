1bd2f7282a5993f8753811a8620a4b2483174002
Syke Zhang <syke.zhang@sap.com>
2021-06-08 10:32:52 +0800

    Add log control and new parameters

--- tool/calc.py
+++ tool/calc.py
@@ -1,18 +1,26 @@
 import math
+import  logging
 from sys import argv
+
+LOG_LEVEL = logging.INFO
+
 def calc_value(years, investment, salary, salary_growth, inflation, bonus):
+    logger = logging.getLogger()
+    logger.setLevel(LOG_LEVEL)
+
     duration_month = years * 12
 
     value = 0
 
     grow_monthly = pow(investment + 1, 1/12)
-    #print(grow_monthly)
+    logger.debug(grow_monthly)
     salary_monthly = salary
     bonus_current = bonus
     for time in range(1, duration_month + 1):
         if 0 == (time - 1) % 12 and 1 != time:
             salary_monthly = salary_monthly * (1 + salary_growth)
             value_this_priod = pow(grow_monthly, duration_month - time) * salary_monthly
+            logger.debug(value_this_priod)
 
         elif 0 == time % 12:
             if time == 12:
@@ -20,19 +28,21 @@ def calc_value(years, investment, salary, salary_growth, inflation, bonus):
             else:
                 bonus_current = bonus_current * (1 + salary_growth)
             value_this_priod = pow(grow_monthly, duration_month - time) * (salary_monthly + bonus_current)
+            logger.debug(value_this_priod)
         
         else:
             value_this_priod = pow(grow_monthly, duration_month - time) * salary_monthly
+            logger.debug(value_this_priod)
 
         value += value_this_priod
-        #print(value)
+        logger.debug(value)
 
     asset = int(value/ pow((1 + inflation), years))
     return asset
 
 if __name__ == '__main__':
     if len(argv) < 6:
-        print("Example Insert: 10 25 10000 11 8 400000")
+        print("Example Insert: 10 20 10000 10 8 8 400000")
         quit()
         
     years = int(argv[1])
@@ -40,10 +50,10 @@ if __name__ == '__main__':
     salary = int(argv[3])
     salary_growth = float(argv[4]) / 100
     inflation = float(argv[5]) / 100
-    bonus = salary * 8
+    bonus = salary * float(argv[6])
 
-    if len(argv) == 7:
-        base = int(argv[6])
+    if len(argv) == 8:
+        base = int(argv[7])
     else:
         base = 0
 
