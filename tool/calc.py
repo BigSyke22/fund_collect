34ce18321dd3334300a79deb1ef1eb1b15529b42
Syke Zhang <syke.zhang@sap.com>
2021-07-13 14:30:48 +0800

    Show the amount of fund changes

--- tool/calc.py
+++ tool/calc.py
@@ -1,44 +1,108 @@
-import math
-import argparse
+"""import sism data into qadb"""
 import logging
-from sys import argv
+import argparse
+import untangle
+import zlib
+import base64
+import json
+from suds.client import Client
+from sqlalchemy.exc import DBAPIError
+from qadb import sqlalchemy_session, transaction
 
-LOG_LEVEL = logging.DEBUG
+# pylint: disable=locally-disabled, line-too-long, invalid-name, not-context-manager, no-member
+_logger = logging.getLogger(__name__)
 
-def calc_value(years, investment, salary, bonus, salary_growth, inflation, base):
-    grow_monthly = pow(investment + 1, 1/12)
 
-    assets = (base * (1 + investment) + salary * bonus + salary / (1 - grow_monthly) * (1 - pow(grow_monthly, 12))) / (1 + inflation)
+def get_servers_hana():
+    """This function returns lpars from the QADB."""
+    session = sqlalchemy_session()
+    with transaction(session):
+        select_statement = """ SELECT "HOST" \
+                               FROM "TESTER"."servers.data.tables::hosts" \
+                               WHERE NOT "HOST" LIKE 'lsh%' \
+                               ORDER BY 1 """
+        rows = session.execute(select_statement)
+        return set([row['host'] for row in rows])
 
-    if 1 == years:
-        print(format(int(assets), ','))
-    else:
-        calc_value(years - 1, investment, salary * (1 + salary_growth) / (1 + inflation), bonus, salary_growth, inflation, assets)
+
+def get_servers_sism(username, password):
+    service_team = {
+        "item": {
+            "Sign":'I',
+            "Option":"EQ",
+            "Low":"00319"
+        }
+    }
+
+    object_type = {
+        "item": {
+            "Sign":'I',
+            "Option":"EQ",
+            "Low":"ZSISM_CL_PHYSICAL"
+        }
+    }
+
+    status = {
+        "item": {
+            "Sign":'I',
+            "Option":"NE",
+            "Low":"TURNDOWN"
+        }
+    }
+
+    client = Client(url='https://pi-internal.wdf.sap.corp/dir/wsdl?p=ic/c613adc7ecf73176b8356ef0ccbbb398&endp_url=https://pi-internal.wdf.sap.corp',
+                username=username, 
+                password=password)
+
+    response = client.service.SmartSearchHead_sync(ServiceTeam = service_team, ObjectType = object_type, Status = status).item[0]["Data"]["GzippedXmlLineItems"]["value"]
+
+    decompressed_response = zlib.decompress(base64.b64decode(response), 16 + zlib.MAX_WBITS)
+
+    server_list = untangle.parse(decompressed_response)
+
+    return set(server_data.SERVER.cdata.encode('utf-8').split('.')[0] for server_data in server_list.asx_abap.asx_values.DATA_NODE.item)
+
+
+def insert(new_hosts):
+    """Do the QADB insert.
+
+    Hosts in SISM not included in QADB will
+    be inserted into QADB.
+    """
+    session = sqlalchemy_session()
+    _logger.debug(new_hosts)
+    with transaction(session):
+        for new_host in new_hosts:
+            _logger.info("insert host %(host)s into qadb", {"host": new_host})
+            insert_host_statement = """ insert into "TESTER"."servers.data.tables::hosts" \
+            values (:hostname,'wdf.sap.corp') """
+            session.execute(insert_host_statement, {'hostname': new_host})
+            insert_label_statement = """ insert into "TESTER"."servers.data.tables::custom_labels" \
+            values (:hostname,'added_by_syncbot') """
+            try:
+                session.execute(insert_label_statement, {'hostname': new_host})
+            except DBAPIError as exc:
+                if exc.orig.errorcode != 301:
+                    raise
+
+
+def main():
+    """main"""
+    handler = logging.StreamHandler()
+    handler.formatter = logging.Formatter('%(asctime)s %(funcName)s %(levelname)s: %(message)s')
+    _logger.addHandler(handler)
+    _logger.setLevel(logging.INFO)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("username")
+    parser.add_argument("password")
+    args = parser.parse_args()
+
+    servers = get_servers_sism(args.username, args.password)
+    servershana = get_servers_hana()
+    newservers = servers - servershana
+    insert(newservers)
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("years")
-    parser.add_argument("investment")
-    parser.add_argument("salary")
-    parser.add_argument("bonus")
-    parser.add_argument("salary_growth")
-    parser.add_argument("inflation")
-    parser.add_argument("-b", "--base", help = "add base")
-
-    args = parser.parse_args()
-        
-    years = int(args.years)
-    investment = float(args.investment) / 100
-    salary = int(args.salary)
-    bonus = float(args.bonus)
-    salary_growth = float(args.salary_growth) / 100
-    inflation = float(args.inflation) / 100
-
-    if args.base:
-        base = int(args.base) * 10000
-    else:
-        base = 0
-
-    calc_value(years, investment, salary, bonus, salary_growth, inflation, base)
-    
+    main()
